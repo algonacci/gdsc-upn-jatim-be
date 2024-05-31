ed0e83f1a104be3b7a8f971fa23f429d06872b28
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2024-05-31 09:24:12 +0700

    streaming request

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const express = require("express");
 const morgan = require("morgan");
 const nunjucks = require("nunjucks");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const cors = require("cors");
 
 const configuration = new GoogleGenerativeAI(process.env.API_KEY);
 const modelId = "gemini-pro";
 const model = configuration.getGenerativeModel({ model: modelId });
 
 const app = express();
+app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "./public")));
 nunjucks.configure("views", {
@@ -33,6 +35,21 @@ app.post("/simple_gemini_api_request", async (req, res) => {
   const result = await model.generateContent(prompt);
   console.log(result.response.text());
   res.render("pages/simpleRequest.njk", { response: result.response.text(), inputPrompt });
+});
+
+app.get("/streaming_request", (req, res) => {
+  res.render("pages/streamingRequest.njk");
+});
+
+app.post("/streaming_request", async (req, res) => {
+  const { inputPrompt } = req.body;
+  const prompt = inputPrompt;
+  const result = await model.generateContentStream(prompt);
+  for await (const chunk of result.stream) {
+    const chunkText = chunk.text();
+    console.log(chunkText);
+    res.write(chunkText);
+  }
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const http = require("http");
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 const start = () => {
